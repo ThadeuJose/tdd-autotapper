24b1f62a9d285ba4f258d47782d17ee005a8cfba
Thadeu Jose <Thadeu@Notebook>
2020-12-24 15:25:25 -0300

    Test and Class implemented
    Test and Class implemented
    Commit done using Eclipse Git

---
+++ tdd-autotapper/src/autotapper/Autotapper.java
@@ -0,0 +1,5 @@
+package autotapper;
+
+public class Autotapper {
+
+}
---
+++ tdd-autotapper/test/autotapper/autotapperTest.java
@@ -0,0 +1,14 @@
+package autotapper;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class autotapperTest extends TestCase {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
