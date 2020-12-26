4021e8521aaa21200458bc0ed9868e264f23ee5a
Thadeu José <thadeujs@gmail.com>
2020-12-26 16:51:27 -0300

    Update Autotapper.java

--- tdd-autotapper/src/autotapper/Autotapper.java
+++ tdd-autotapper/src/autotapper/Autotapper.java
@@ -2,12 +2,24 @@ package autotapper;
 
 public class Autotapper {
 	
-	public static boolean tapper(Board board, String cardManaCost) {
-		if(board.canGenerate(cardManaCost)) {
-			board.tap(cardManaCost);
-			return true;
+	public static Response tapper(Board board, String cardManaCost) {
+		boolean isCastable;
+		Board newBoard = board.valueOf();
+		String manaCost = cardManaCost; //TransformManaCost.transform(cardManaCost);
+		for (int i = 0; i < manaCost.length(); i++) {
+			String symbol = String.valueOf(cardManaCost.charAt(i));
+			if(!board.canGenerate(symbol)) {
+				isCastable = false;
+				return createResponse(isCastable,newBoard);				
+			}
+			newBoard.tap(symbol);			
 		}
-		return false;		
+		isCastable = true;
+		return createResponse(isCastable, newBoard.valueOf());		
 	}
-
+	
+	private static Response createResponse(boolean isCastable, Board board) {
+		return new Response(isCastable, board);
+	}
+	
 }
